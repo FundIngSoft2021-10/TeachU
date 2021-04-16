b2201fdf254f35d430f7037a914f823662f142d4
juan <juanhit471523@gmail.com>
2021-04-16 17:25:14 -0500

    hola

--- src/app/src/main/java/activities/Login.java
+++ src/app/src/main/java/activities/Login.java
@@ -2,7 +2,11 @@ package activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.teachu.R;
 
 public class Login extends AppCompatActivity {
@@ -11,5 +15,12 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn_registrarse = findViewById(R.id.BTN_registro);
+        btn_registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registro = new Intent(Login.this, Registro.class);
+            }
+        });
     }
 }
