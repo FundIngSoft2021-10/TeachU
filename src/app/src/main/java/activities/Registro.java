f6921f89f311d73978ee02e60a47ea1c340e256f
juan <juanhit471523@gmail.com>
2021-04-16 15:58:52 -0500

    interface

--- src/app/src/main/java/activities/Registro.java
+++ src/app/src/main/java/activities/Registro.java
@@ -2,7 +2,12 @@ package activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
 import com.example.teachu.R;
 public class Registro extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        RadioButton re,rt;
+        Button btn_registro = findViewById(R.id.btn_registro);
+        re = findViewById(R.id.radio_Estudiante);
+        rt = findViewById(R.id.radio_Tutor);
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(re.isChecked()){
+                    Intent int_registroEst = new Intent(Registro.this,Registro_Estudiante.class);
+                    startActivity(int_registroEst);
+                }
+            }
+        });
     }
 }
---
+++ src/app/src/main/java/activities/Registro_Estudiante.java
@@ -0,0 +1,15 @@
+package activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.teachu.R;
+
+public class Registro_Estudiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro__estudiante);
+    }
+}
