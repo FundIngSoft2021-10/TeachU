9f21efafa9b64f91ba91c23a1ec869ec59e614e8
juan <juanhit471523@gmail.com>
2021-04-18 16:53:13 -0500

    lista carreras

--- src/app/src/main/java/activities/Registro_Estudiante.java
+++ src/app/src/main/java/activities/Registro_Estudiante.java
@@ -3,13 +3,61 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.teachu.R;
 
-public class  Registro_Estudiante extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class  Registro_Estudiante extends AppCompatActivity {
+    List<String> carreras;
+    Spinner spinnercarrera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro__estudiante);
+        carreras = new ArrayList<String>();
+        spinnercarrera = findViewById(R.id.spinner);
+        RetornarCarreras("http://192.168.100.137:80/webservices/prueba.php");
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,carreras);
+        spinnercarrera.setAdapter(adaptador);
+    }
+    private void RetornarCarreras(String URL){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        carreras.add(jsonObject.getString("Ncarrera"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "ERROR DE CONEXION", Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
