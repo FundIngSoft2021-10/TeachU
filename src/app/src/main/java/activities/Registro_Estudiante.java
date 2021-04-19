1ff4297efbf1d86b0d53af7af03c554fed3c717b
juan <juanhit471523@gmail.com>
2021-04-18 20:38:39 -0500

    XD

--- src/app/src/main/java/activities/Registro_Estudiante.java
+++ src/app/src/main/java/activities/Registro_Estudiante.java
@@ -3,12 +3,17 @@ package activities;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.teachu.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -16,18 +21,21 @@ import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
 public class  Registro_Estudiante extends AppCompatActivity {
     Spinner listado;
-
+    String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro__estudiante);
         listado = (Spinner) findViewById(R.id.spinnercarrera);
         obtDatos();
+        String id_usuario = getIntent().getExtras().getString("NombreU");
+
 
 
     }
@@ -35,7 +43,7 @@ public class  Registro_Estudiante extends AppCompatActivity {
 
     public void obtDatos() {
         AsyncHttpClient cliente = new AsyncHttpClient();
-        String url = "http://192.168.0.28:80/webservices/prueba.php";
+        String url = "http://192.168.100.137:80/webservices/prueba.php";
         RequestParams parametros = new RequestParams();
         parametros.put("clase", 18);
 
@@ -73,7 +81,28 @@ public class  Registro_Estudiante extends AppCompatActivity {
     }
 
     private void buscarId(String URL){
-        JsonArrayRequest jsonArrayRequest
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        id = jsonObject.getString("Id_Usuario");
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"ERROR DE CONEXION",Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 
 }
