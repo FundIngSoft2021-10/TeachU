3ca3af10226826f870187b0c1b42a171155cbe0c
juan <juanhit471523@gmail.com>
2021-05-17 20:41:25 -0500

    Recuperacion

---
+++ src/app/src/main/java/activities/recuperacion_de_contrasena.java
@@ -0,0 +1,14 @@
+package activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.teachu.R;
+import android.os.Bundle;
+
+public class recuperacion_de_contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperacion_de_contrasena);
+    }
+}
