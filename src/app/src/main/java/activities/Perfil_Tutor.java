e5637289aa170d1b75a10d8dc952b861d606b58a
juan <juanhit471523@gmail.com>
2021-05-25 16:17:12 -0500

    :'(

--- src/app/src/main/java/activities/Perfil_Tutor.java
+++ src/app/src/main/java/activities/Perfil_Tutor.java
@@ -67,7 +67,6 @@ public class Perfil_Tutor extends AppCompatActivity {
                         ran = rank.get(j);
                     }
                 }
-
                 descripcion.setText(des);
             }
         });
