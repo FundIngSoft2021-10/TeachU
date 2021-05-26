387a9682495110b5ede8b22b78012fd2269374c1
Daniel Traslavina <danstervan1@gmail.com>
2021-05-26 15:35:56 -0700

    cambios include

---
+++ webservices/include/TutoriasXUsuarios.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'include/DB_Connect.php';
+$dd = new DB_Connect();
+$con = $dd->conectar();
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+	
+$query = $con->prepare("Select * FROM TutoriaXEstudiante NATURAL JOIN TutoriaXTutor");
+
+$sql ="Select * FROM TutoriaXEstudiante NATURAL JOIN TutoriaXTutor";
+$result = mysqli_query($con, $sql);
+$rows = array();
+	
+if(mysqli_num_rows($result) > 0){
+	while($r = mysqli_fetch_assoc($result)){
+		array_push($rows, $r);
+	}
+	print json_encode($rows);
+}
+	
+	
+?>
